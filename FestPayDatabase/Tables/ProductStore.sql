7c2f98690f63d523571ccb298b56f19d6987b409
Akitektuo <alexcopindean@yahoo.com>
2017-07-01 14:09:30 +0300

    Added Web

--- FestPayDatabase/Tables/ProductStore.sql
+++ FestPayDatabase/Tables/ProductStore.sql
@@ -4,6 +4,7 @@
     [ProductId] INT NOT NULL, 
     [StoreId] INT NOT NULL, 
     [PriceModifier] DECIMAL(5, 5) NOT NULL DEFAULT 1,
-	CONSTRAINT [FK_ProductStore_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]),
+	[Stock] INT NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_ProductStore_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]),
 	CONSTRAINT [FK_ProductStore_Store] FOREIGN KEY ([StoreId]) REFERENCES [Store]([Id])
 )
---
+++ FestPayWeb/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace FestPayWeb
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddMvc()
+                .AddJsonOptions(o =>
+                {
+                    o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseApplicationInsightsRequestTelemetry();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            //app.UseStatusCodePagesWithReExecute("/home/Index");
+
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    "Wildcard Routing",
+                    "{*anything}",
+                    new { controller = "Home", action = "Index" });
+            });
+        }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+
+    }
+}
