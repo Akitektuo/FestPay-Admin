9c82e84d2ab97545dfd501d9b5699b03848d722f
Akitektuo <alexcopindean@yahoo.com>
2017-07-09 13:49:08 +0300

    A lot of changes :)

--- FestPay.Web/Controllers/HomeController.cs
+++ FestPay.Web/Controllers/HomeController.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using FestPayWeb.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace FestPayWeb.Controllers
+namespace FestPay.Web.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Index(UserModel user)
+        public IActionResult About()
         {
-            var x = user.Email;
+            ViewData["Message"] = "Your application description page.";
+
             return View();
         }
 
-        public IActionResult Register()
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
         {
             return View();
         }
--- FestPayWeb/Models/UserModel.cs
+++ FestPayWeb/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,7 @@ namespace FestPayWeb.Models
     {
         public string Email { get; set; }
         public string Password { get; set; }
+        [DisplayName("Remember me")]
+        public bool Remember { get; set; }
     }
 }
