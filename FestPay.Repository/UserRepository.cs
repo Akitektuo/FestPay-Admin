7044a381927359b50c2353de121d3caf52c06b35
Marius-Claudiu Pop <marius1741@yahoo.com>
2017-08-28 20:04:21 +0300

    DB connection example

---
+++ FestPay.Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestPay.Repository
+{
+    public class UserRepository
+    {
+        public FestPayEntities EntityContext { get; private set; }
+
+        public UserRepository(FestPayEntities entityContext)
+        {
+            EntityContext = entityContext;
+        }
+
+        public User GetUserById(int userId)
+        {
+            var user = EntityContext.Users.FirstOrDefault(u => u.Id == userId);
+            return user;
+        }
+
+        public User Add(User user)
+        {
+            EntityContext.Users.Add(user);
+            EntityContext.SaveChanges();
+
+            var newUser = EntityContext.Users.Last();
+            return newUser;
+        }
+    }
+}
