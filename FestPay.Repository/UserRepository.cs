b8328ab346e8769cc10f7a7134fb6bc4f7a4cc0f
Marius-Claudiu Pop <marius1741@yahoo.com>
2017-08-28 20:10:25 +0300

    DB modify example

--- FestPay.Repository/UserRepository.cs
+++ FestPay.Repository/UserRepository.cs
@@ -29,5 +29,19 @@ namespace FestPay.Repository
             var newUser = EntityContext.Users.Last();
             return newUser;
         }
+
+        public User Modify(User user)
+        {
+            var trackedUser = EntityContext.Users.FirstOrDefault(u => u.Id == user.Id);
+
+            trackedUser.Email = user.Email;
+            
+            //for entity framework settings that have untracked changes
+            //EntityContext.Entry(trackedUser).State = System.Data.Entity.EntityState.Modified;
+
+            EntityContext.SaveChanges();
+
+            return trackedUser;
+        }
     }
 }
