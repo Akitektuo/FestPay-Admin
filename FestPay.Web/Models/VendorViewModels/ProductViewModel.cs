8a5d952988e05b7ea48dee5900608e24e52a8d86
Akitektuo <alexcopindean@yahoo.com>
2017-09-15 21:53:13 +0300

    Refactored shops from ProductViewModel

--- FestPay.Web/Models/VendorViewModels/ProductViewModel.cs
+++ FestPay.Web/Models/VendorViewModels/ProductViewModel.cs
@@ -10,7 +10,7 @@ namespace FestPay.Web.Models.VendorViewModels
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
-        public List<string> Shops { get; set; }
+        public string Shop { get; set; }
         public string Image { get; set; }
         public string ImageURL { get; set; }
     }
